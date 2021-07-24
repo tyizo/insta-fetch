117a821e687ea6457212ca64d05f523832232e16
Tyizo <69811071+tyizo@users.noreply.github.com>
2021-07-24 20:48:34 +0430

    Update test.js

--- example/test.js
+++ example/test.js
@@ -1,2 +1,3 @@
-const fetchUser = require("../instagram");
-fetchUser("1xm0d");
+const { fetchUser, fetchPost } = require("./insta-Packge");
+const data = fetchUser("1xm0d").then((data) => console.log(data));
+const data2 = fetchPost("CRtrth5oJTV").then((data) => console.log(data));
